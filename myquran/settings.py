ad5a4da89f086a6d3b4dfc28b2b4034653be11f6
Habib <habibliadi5@gmail.com>
2025-11-30 20:00:10 +0100

    Fix production issues: Email Config

--- myquran/settings.py
+++ myquran/settings.py
@@ -99,9 +99,9 @@ EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.conso
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Al-Qur\'an <noreply@alquran.com>')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='habibliadi5@gmail.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='wvdj ptsx bgen ytzq')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='Al-Qur\'an <habibliadi5@gmail.com>')
 
 # Internationalization
 LANGUAGE_CODE = 'en-us'
