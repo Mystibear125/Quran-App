48f5a2f68fe4ac7c7828cc895aa8ce89f9c4c2fe
Habib <habibliadi5@gmail.com>
2025-11-30 15:47:05 +0100

    Add auto superuser creation

--- myquran/settings.py
+++ myquran/settings.py
@@ -171,27 +171,3 @@ if not DEBUG:
     
     # Additional Security
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-
-from django.apps import apps
-
-if os.environ.get("AUTO_CREATE_SUPERUSER") == "1":
-
-    def create_superuser():
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-        if username and not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password
-            )
-
-    # Run after apps are ready
-    if apps.ready:
-        create_superuser()
