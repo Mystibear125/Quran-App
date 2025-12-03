c47ced020b6fe9801b7ab1f22bdabf7cdaeef5a5
Habib <habibliadi5@gmail.com>
2025-12-03 15:17:00 +0100

    static

--- myquran/settings.py
+++ myquran/settings.py
@@ -113,6 +113,9 @@ USE_TZ = True
 # ==================== STATIC FILES ====================
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
